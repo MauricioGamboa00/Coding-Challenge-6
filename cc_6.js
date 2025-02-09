da3de363264043b5c47d8a60e39a336838057219
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-09 13:11:39 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -4,11 +4,22 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
     let profit = (sellingPrice - costPrice) * unitsSold;
     return profit;
 
-}
+}; // Created Formula to calculate profit
 
 calculateProfit(20, 30, 100); // Expected Value $1000
 calculateProfit(50, 70, 200); // Expected Value $4000
 
-console.log("Profit Example 1:", calculateProfit(20, 30, 100));
-console.log("Profit Example 2:", calculateProfit(50, 70, 200));
+console.log("Total Profit: $", calculateProfit(20, 30, 100)); // Logging the result of the function 
+console.log("Total Profit: $", calculateProfit(50, 70, 200)); // Logging the result of the function
+
+// Task 2 Function Expression
+
+let calculateSalesTax = function(amount, taxRate) {
+    let salesTax =Math.floor(amount * taxRate);
+    return salesTax;
+};
+
+console.log("Sales Tax: $", calculateSalesTax(100, 0.07)); // Expected Value $7
+console.log("Sales Tax: $", calculateSalesTax(500, 0.01)); // Expected Value $5
+
 
