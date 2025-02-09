f14eb9f77372611d6c0dbe4f1f42510fff83800b
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-09 13:30:48 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -4,7 +4,7 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
     let profit = (sellingPrice - costPrice) * unitsSold;
     return profit;
 
-}; // Created Formula to calculate profit
+}; // Created function to calculate profit
 
 calculateProfit(20, 30, 100); // Expected Value $1000
 calculateProfit(50, 70, 200); // Expected Value $4000
@@ -17,9 +17,26 @@ console.log("Total Profit: $", calculateProfit(50, 70, 200)); // Logging the res
 let calculateSalesTax = function(amount, taxRate) {
     let salesTax =Math.floor(amount * taxRate);
     return salesTax;
-};
+}; // Created a function to calculate sales tax
 
 console.log("Sales Tax: $", calculateSalesTax(100, 0.07)); // Expected Value $7
 console.log("Sales Tax: $", calculateSalesTax(500, 0.01)); // Expected Value $5
 
+
+// Task 3 Arrow Function 
+
+let calculateBonus = (salary, performanceRating) => {
+    let bonus;
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.20; // Bonus is 20% of salary
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.10; // Bonus is 10% of salary
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05; // Bonus is 5% of salary
+    }
+    return bonus;
+};
+
+console.log("Bonus 1: $", calculateBonus (5000, "Excellent")); // logging bonus with 5000 salary and excellent performance rating
+console.log("Bonus 2: $", calculateBonus (7000, "Good")); // logging bonus with 7000 salary and good performance rating
 
